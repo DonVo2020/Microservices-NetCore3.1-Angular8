25ee5d8776ee2dff32532b4f2bb443bd1587f55a
Don Vo - NC - USA <59782883+DonVo2020@users.noreply.github.com>
2020-01-17 18:12:19 -0500

    Add AdminService project

---
+++ DonVo.MicroservicesNetCore31.Year2020/DonVo.AdminService/Controllers/AdminController.cs
@@ -0,0 +1,102 @@
+﻿using DonVo.AdminDomain.Repositories;
+using DonVo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MOD.AdminService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        readonly IAdminRepository repository;
+        public AdminController(IAdminRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // POST: api/Admin
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Post([FromBody] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                bool result = await repository.AddCoursesAsync(course);
+                if (result)
+                {
+                    return Created("AddCourse", course);
+                }
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // PUT: api/Admin/5
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Put(int id, [FromBody] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                bool result = await repository.UpdateCourseAsync(course);
+                if (result)
+                {
+                    return Created("UpdatedCourse", course.Id);
+                }
+            }
+            return BadRequest(ModelState);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var course = await repository.GetCourseAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            bool result = await repository.DeleteCourseAsync(course);
+            if (result)
+            {
+                return Ok();
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+        [HttpGet("usersList")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetUsersList()
+        {
+            return Ok(await repository.GetUsersListAsync());
+        }
+
+        [HttpGet("mentorsList")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetMentorsList()
+        {
+            return Ok(await repository.GetMentorsListAsync());
+        }
+        [HttpGet("blockunblock/{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetBlockUnblock(string id)
+        {
+            var result = await repository.BlockUserAsync(id);
+            if (result)
+            {
+                return Ok();
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+        // GET: api/Admin
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetEnrolledCourses()
+        {
+            return Ok(await repository.GetEnrolledCoursesAsync());
+        }
+    }
+}
---
+++ DonVo.MicroservicesNetCore31.Year2020/DonVo.AdminService/Startup.cs
@@ -0,0 +1,101 @@
+using DonVo.AdminDomain;
+using DonVo.AdminDomain.Repositories;
+using DonVo.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace DonVo.AdminService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddDbContext<AdminContext>(options =>
+                options.UseSqlServer(
+                Configuration.GetConnectionString("SqlConnectionString")));
+
+            services.AddIdentity<User, IdentityRole>()
+                 .AddEntityFrameworkStores<AdminContext>()
+               .AddDefaultTokenProviders();
+
+            services.AddControllers();
+            services.AddScoped<IAdminRepository, AdminRepository>();
+
+            //JWT
+            JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme =
+                JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme =
+                JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme =
+                JwtBearerDefaults.AuthenticationScheme;
+
+            })
+            .AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = Configuration["JwtIssuer"],
+                    ValidAudience = Configuration["JwtIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                          Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            services.AddMvc(option => option.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Latest)
+                .AddNewtonsoftJson(opt =>
+                opt.SerializerSettings.ReferenceLoopHandling =
+                ReferenceLoopHandling.Ignore);
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors(policy =>
+                        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
