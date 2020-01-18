3cf0f6403be4b9ffd868371e0f77f8246ae4f06e
Don Vo - NC - USA <59782883+DonVo2020@users.noreply.github.com>
2020-01-17 19:57:56 -0500

    Add DonVo.MentorService project

---
+++ DonVo.MicroservicesNetCore31.Year2020/DonVo.MentorService/Controllers/MentorController.cs
@@ -0,0 +1,64 @@
+﻿using DonVo.DTOs;
+using DonVo.MentorDomain.Repositories;
+using DonVo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DonVo.MentorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MentorController : ControllerBase
+    {
+        readonly IMentorRepository repository;
+        public MentorController(IMentorRepository repository)
+        {
+            this.repository = repository;
+        }
+        [HttpGet("mentorProfile/{email}")]
+        [Authorize(Roles = "Mentor")]
+        public async Task<IActionResult> MentorProfileDetails(string email)
+        {
+            var result = await repository.MentorProfileDetailsAsync(email);
+            return Ok(result);
+        }
+
+        [HttpPut("mentorProfile/{mentorId}")]
+        [Authorize(Roles = "Mentor")]
+        public async Task<IActionResult> UpdateMentorDetails(string mentorId, [FromBody] ProfileDto mentorData)
+        {
+            if (ModelState.IsValid)
+            {
+                bool result = await repository.UpdateMentorDetailsAsync(mentorData, mentorId);
+                if (result)
+                {
+                    return Created("UpdatedProfie", null);
+                }
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet("ListOfCourseMentor/{modelEmail}")]
+        [Authorize(Roles = "Mentor")]
+        public async Task<IActionResult> GetEnrolledCoursesByMentor(string modelEmail)
+        {
+            var result = await repository.GetEnrolledCoursesByMentorAsync(modelEmail);
+            return Ok(result);
+        }
+        [HttpPut("ChangeEnrolledCourseStatus/{id}/{UserEmail}")]
+        [Authorize(Roles = "Mentor")]
+        public async Task<IActionResult> ChangeCourseStatus(int id, string UserEmail, [FromBody] EnrolledCourse enrolledCourse)
+        {
+            if (ModelState.IsValid && id == enrolledCourse.Id)
+            {
+                bool result = await repository.ChangeCourseStatusAsync(enrolledCourse, UserEmail);
+                if (result)
+                {
+                    return Created("UpdatedCourse", enrolledCourse.Id);
+                }
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
