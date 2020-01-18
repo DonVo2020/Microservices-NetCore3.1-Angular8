815608f8029c0cca9a8e1d12cbc1521cd59a3db0
Don Vo - NC - USA <59782883+DonVo2020@users.noreply.github.com>
2020-01-17 19:37:35 -0500

    Add DonVo.CourseService project

---
+++ DonVo.MicroservicesNetCore31.Year2020/DonVo.CourseService/Controllers/CourseController.cs
@@ -0,0 +1,31 @@
+﻿using DonVo.CourseDomain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MOD.CourseService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        readonly ICourseRepository repository;
+        public CourseController(ICourseRepository repository)
+        {
+            this.repository = repository;
+        }
+        // GET: api/Course
+        [HttpGet]
+        //[Authorize(Roles = "Admin,Student,Mentor")]
+        public async Task<IActionResult> GetCourses()
+        {
+            return Ok(await repository.GetCoursesAsync());
+        }
+        [HttpGet("search/{criteria}")]
+        public async Task<IActionResult> SearchCourse(string criteria)
+        {
+            var result = await repository.SearchCourseAsync(criteria);
+            return Ok(result);
+        }
+
+    }
+}
---
+++ DonVo.MicroservicesNetCore31.Year2020/DonVo.CourseService/Startup.cs
@@ -0,0 +1,101 @@
+using DonVo.CourseDomain;
+using DonVo.CourseDomain.Repositories;
+using DonVo.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace DonVo.CourseService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddDbContext<CourseContext>(options =>
+                options.UseSqlServer(
+                Configuration.GetConnectionString("SqlConnectionString")));
+
+            services.AddIdentity<User, IdentityRole>()
+                 .AddEntityFrameworkStores<CourseContext>()
+               .AddDefaultTokenProviders();
+
+            services.AddControllers();
+            services.AddScoped<ICourseRepository, CourseRepository>();
+
+            //JWT
+            JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme =
+                JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme =
+                JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme =
+                JwtBearerDefaults.AuthenticationScheme;
+
+            })
+            .AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = Configuration["JwtIssuer"],
+                    ValidAudience = Configuration["JwtIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                          Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            services.AddMvc(option => option.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                .AddNewtonsoftJson(opt =>
+                opt.SerializerSettings.ReferenceLoopHandling =
+                ReferenceLoopHandling.Ignore);
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors(policy =>
+                        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
