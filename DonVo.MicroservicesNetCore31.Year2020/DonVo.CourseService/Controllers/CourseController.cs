93dcb31ac9b53158546605db2d796e2777d0d38c
Don Vo - NC - USA <59782883+DonVo2020@users.noreply.github.com>
2020-01-17 19:53:18 -0500

    Update namespace

--- DonVo.MicroservicesNetCore31.Year2020/DonVo.CourseService/Controllers/CourseController.cs
+++ DonVo.MicroservicesNetCore31.Year2020/DonVo.CourseService/Controllers/CourseController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
-namespace MOD.CourseService.Controllers
+namespace DonVo.CourseService.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
