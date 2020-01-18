596915f3151e311f339f5fc7f4c2e8305b750ddc
Don Vo - NC - USA <59782883+DonVo2020@users.noreply.github.com>
2020-01-17 20:45:10 -0500

    Add DonVo.ApiGateway project

---
+++ DonVo.MicroservicesNetCore31.Year2020/DonVo.ApiGateway/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Threading.Tasks;
+
+namespace DonVo.ApiGateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers();
+            services.AddOcelot();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public async Task ConfigureAsync(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(policy => policy.AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .AllowAnyOrigin());
+
+            app.UseRouting();
+            //app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            await app.UseOcelot();
+        }
+    }
+}
